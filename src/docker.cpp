4b9c3af4dbc3a4831d149d7bfbff0e88f65ca6f5
Ander Arbelaiz <arbelaitz@gmail.com>
2020-03-22 14:16:05 +0100

    Refactoring...

--- src/docker.cpp
+++ src/docker.cpp
@@ -36,10 +36,11 @@ DockerError Docker::images(imageList &result, bool all, bool digests)
 {
 	const std::string url = _endpoint + "/images/json?all=" + BoolToText(all) + "?digests=" + BoolToText(digests);
 	auto res = Http::get(asl::String(url.c_str()));
-	if (!res.ok()) return _checkError(&res);
+	DockerError err = _checkError(&res);
+	if (!err.isOk()) return err;
 	auto data = res.json();
 	parseImages(&data, result);
-	return DockerError::OK();
+	return err;
 }
 
 DockerError Docker::containers(containerList &result, bool all, int limit, bool size)
@@ -47,43 +48,40 @@ DockerError Docker::containers(containerList &result, bool all, int limit, bool 
 	std::string url = _endpoint + "/containers/json?all=" + BoolToText(all) + "?size=" + BoolToText(size);
 	if (limit > 0) url += url + "?limit=" + std::to_string(limit);
 	auto res = Http::get(asl::String(url.c_str()));
-	if (!res.ok()) return _checkError(&res);
+	DockerError err = _checkError(&res);
+	if (!err.isOk()) return err;
 	asl::String filteredResponse = res.text().replace("\\\"", ""); // AAA: scaping is necessary for commands
 	asl::Var data = asl::Json::decode(filteredResponse);
 	parseContainers(&data, result);
-	return DockerError::OK();
+	return err;
 }
 
 DockerError Docker::startContainer(const std::string &id, const std::string &detachKeys)
 {
 	const std::string url = _endpoint + "/containers/" + id + "/start/json?detachKeys=" + detachKeys;
 	auto res = Http::get(asl::String(url.c_str()));
-	if (!res.ok()) return _checkError(&res);
-	return DockerError::OK();
+	return _checkError(&res);
 }
 
 DockerError Docker::stopContainer(const std::string &id, int t)
 {
 	const std::string url = _endpoint + "/containers/" + id + "/stop/json?t=" + std::to_string(t);
 	auto res = Http::get(asl::String(url.c_str()));
-	if (!res.ok()) return _checkError(&res);
-	return DockerError::OK();
+	return _checkError(&res);
 }
 
 DockerError Docker::restartContainer(const std::string &id, int t)
 {
 	const std::string url = _endpoint + "/containers/" + id + "/restart/json?t=" + std::to_string(t);
 	auto res = Http::get(asl::String(url.c_str()));
-	if (!res.ok()) return _checkError(&res);
-	return DockerError::OK();
+	return _checkError(&res);
 }
 
 DockerError Docker::killContainer(const std::string &id, const std::string &signal)
 {
 	const std::string url = _endpoint + "/containers/run/" + id + "/kill/json?signal=" + signal;
 	auto res = Http::get(asl::String(url.c_str()));
-	if (!res.ok()) return _checkError(&res);
-	return DockerError::OK();
+	return _checkError(&res);
 }
 
 bool Docker::checkConnection()
@@ -94,7 +92,16 @@ bool Docker::checkConnection()
 
 DockerError Docker::_checkError(asl::HttpResponse *res)
 {
-	return DockerError::ERROR(*(res->json()["message"].toString()), res->code());
+	int code = res->code();
+	if (code == 200 || code == 204) {
+		return DockerError::OK();
+	} else if (code < 400) {
+		std::string msg = *(res->json()["message"].toString());
+		return DockerError::INFO(msg, code); 
+	}else{
+		std::string msg = *(res->json()["message"].toString());
+		return DockerError::ERROR(msg, code);
+	}	
 }
 
 }
