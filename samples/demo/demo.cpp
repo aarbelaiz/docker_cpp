113939c85bd03549bcb94e4cc65687bcf193c317
Ander Arbelaiz <arbelaitz@gmail.com>
2020-06-16 00:53:33 +0200

    A little bit of pretty print

--- samples/demo/demo.cpp
+++ samples/demo/demo.cpp
@@ -1,6 +1,7 @@
 #include "docker_cpp/docker.h"
 
 #include <iostream>
+#include <iomanip>
 
 using namespace docker_cpp;
 
@@ -32,8 +33,18 @@ int main()
 	ImageList res;
 	err = docker.images(res);
 	if (err.isOk()) {
-		std::cout << res[0].id << '\n';
-		std::cout << res[0].sharedSize << '\n';
+		std::cout << "--- IMAGES ----" << '\n';
+		std::cout << std::left << std::setw(75) << "IMAGE ID" << std::left << std::setw(35) << "REPO" 
+				  << std::left << std::setw(35) << "LABEL" << std::left << std::setw(15) << "CREATED"
+				  << std::left << std::setw(15) << "SIZE" << '\n';
+		for(auto &img : res) {
+			std::string repos = (!img.repoTags.empty()) ? img.repoTags[0] : "<none>";
+			std::string labels = (!img.labels.empty()) ? img.labels[0].first + ":" + img.labels[0].second : "<none>";
+			std::cout << std::left << std::setw(75) << img.id << std::left << std::setw(35) << repos
+					  << std::left << std::setw(35) << labels << std::left << std::setw(15) << img.created
+					  << std::left << std::setw(15) << img.size << '\n';
+		}
+		std::cout << "---------------" << '\n';
 	}
 
 	// List containers
@@ -43,10 +54,14 @@ int main()
 		std::cout << err << '\n';
 		return EXIT_FAILURE;
 	}
+	std::cout << "--- CONTAINERS ----" << '\n';
+	std::cout << std::left << std::setw(70) << "CONTAINER ID" << std::left << std::setw(40) << "NAME"
+			  << std::left << std::setw(40) << "STATUS" << std::left << std::setw(40) << "IMAGE" << '\n';
 	for(auto &c : containers) {
-		std::cout << c.id << '\n';
-		std::cout << c.names[0] << '\n';
+		std::cout << std::left << std::setw(70) << c.id << std::left << std::setw(40) << c.names[0]
+				  << std::left << std::setw(40) << c.status << std::left << std::setw(40) << c.image << '\n';
 	}
+	std::cout << "--------------------" << '\n';
 
 	return EXIT_SUCCESS;
 }
